62fc6693815327c6cc6b2d1eb2c42b08efc22315
Ali Salim Rashid <a.rashid@zantekk.com>
2014-09-18 12:21:01 +0300

    Regenerate file headers.

--- src/main/scala/codes/reactive/scalatime/temporal/RichTemporal.scala
+++ src/main/scala/codes/reactive/scalatime/temporal/RichTemporal.scala
@@ -1,20 +1,20 @@
-/*******************************************************************
- * See the NOTICE file distributed with this work for additional   *
- * information regarding Copyright ownership.  The author/authors  *
- * license this file to you under the terms of the Apache License, *
- * Version 2.0 (the "License"); you may not use this file except   *
- * in compliance with the License.  You may obtain a copy of the   *
- * License at:                                                     *
- *                                                                 *
- *     http://www.apache.org/licenses/LICENSE-2.0                  *
- *                                                                 *
- * Unless required by applicable law or agreed to in writing,      *
- * software distributed under the License is distributed on an     *
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY          *
- * KIND, either express or implied.  See the License for the       *
- * specific language governing permissions and limitations         *
- * under the License.                                              *
- *******************************************************************/
+/******************************************************************
+ * See the NOTICE file distributed with this work for additional  *
+ * information regarding Copyright ownership.  The author/authors *
+ * license this file to you under the terms of the Apache License *
+ * Version 2.0 (the "License"); you may not use this file except  *
+ * in compliance with the License.  You may obtain a copy of the  *
+ * License at:                                                    *
+ *                                                                *
+ *     http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                                *
+ * Unless required by applicable law or agreed to in writing,     *
+ * software distributed under the License is distributed on an    *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,   *
+ * either express or implied.  See the License for the specific   *
+ * language governing permissions and limitations under the       *
+ * License.                                                       *
+ ******************************************************************/
 
 package codes.reactive.scalatime
 package temporal
@@ -75,7 +75,7 @@ final class RichTemporal(val underlying: Temporal) extends AnyVal {
     * @throws DateTimeException - if unable to query
     * @throws ArithmeticException - if numeric overflow occurs
     */
-  def ? [A](query: TemporalQuery[A]): A = underlying.query(query)
+  def ?[A](query: TemporalQuery[A]): A = underlying.query(query)
 
   /** Calculates the amount of time until another temporal in terms of the specified unit.
     *
